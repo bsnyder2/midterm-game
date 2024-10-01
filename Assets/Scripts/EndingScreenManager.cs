b6c187122698340d0214336e8ad4e3a3a3305430
alyho <alyssachangho@gmail.com>
2024-10-02 00:12:40 +0200

    Ending Screen Fade In and Application Quit on space

---
+++ Assets/Scripts/EndingScreenManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScreenManager : MonoBehaviour
+{
+    public GameObject text;
+    private SpriteRenderer textSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textSpriteRenderer = text.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeAnimator.FadeIn(textSpriteRenderer, 0, 1, 3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Application.Quit();
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
